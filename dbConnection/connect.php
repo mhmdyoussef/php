2e6a78f28419a34f9d4cb6c44f1d094843a58ba3
mhmdYoussef <egsyntax@gmail.com>
2021-04-03 09:24:00 +0200

    updated

--- dbConnection/connect.php
+++ dbConnection/connect.php
@@ -1,42 +1,34 @@
 <?php
-//Database Details
+
+/*** Definaions */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'Admin123!!');
 define('DB_NAME', 'simpledb');
 
-//PDO Connection
+/*** Connection */
 try {
-    $options = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND=> 'SET NAMES UTF8',
+    $dbOptions = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
     );
-    $dsn = "mysql:host=" . DB_HOST . ";dbname=" .DB_NAME;
-    $dbConnect = new PDO ($dsn, DB_USER, DB_PASS, $options);
+    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+    $dbConnect = new PDO ($dsn, DB_USER, DB_PASS, $dbOptions);
     $dbConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOExeption $e) {
+    exit("ERORR: Please connect your system developer ");
 }
-catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
-}
 
+$sql = "INSERT INTO users (`name`, `phone`, `city`, `date_added`) VALUES (:name, :phone, :city, :date)";
+$query = $dbConnect-> prepare($sql);
 
-// Query
-$sql = "SELECT * FROM users";
-$query = $dbConnect->prepare($sql);
+$query->bindParam(':name', $name, PDO::PARAM_STR);
+$query->bindParam(':phone', $phone, PDO::PARAM_INT);
+$query->bindParam(':city', $city, PDO::PARAM_STR);
+$query->bindParam(':date', $date, PDO::PARAM_STR);
 
-// Binding VARs
-//$query->bindParam(':city', $city, PDO::PARAM_STR);
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$city = $_POST['city'];
+$date = date('Y-m-d h:m:s');
 
-// Input Values
-$city = "Cairo";
-
-// Execute
-$query->execute();
-
-//Pulled Data
-$results = $query->fetchAll(PDO::FETCH_OBJ);
-
-if ($query ->rowCount() > 0) {
-    foreach ($results as $result) {
-        echo $result->name . ", " . $result->phone . ", " . $result->city ."<br />";
-    }
-}
+$query-> execute();

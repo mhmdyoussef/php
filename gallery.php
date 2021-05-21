65cc316ac70fb737deca8e0c6c8583237a3f9dd4
mhmdyoussef <71203268+mhmdyoussef@users.noreply.github.com>
2021-05-21 15:49:14 +0200

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_error', 1);
+
+function reader($directory, array $excludeFiles = ['.', '..'])
+{
+    $images = [];
+    if (!is_dir($directory)) {
+        return NULL;
+    }
+    if (!$fileList = opendir($directory)) {
+        return NULL;
+    }
+
+    while (($fileNames = readdir($fileList)) == true)
+    {
+        if (in_array($fileNames, $excludeFiles))
+        {
+            continue;
+        }
+        $images[] = $directory . '/' . $fileNames;
+    }
+
+    closedir($fileList);
+
+    return $images;
+}
+
+$images = reader('img');
+
+
+if (!$images)
+{
+    die("Images couldn't found");
+}
+
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Image Gallery</title>
+    </head>
+    <body>
+    <?php
+        foreach ($images as $image)
+        {
+            echo '<img src="' . $image . '" alt="" width="200px" height="200px"/>';
+        }
+    ?>
+    </body>
+</html>
